8b3cc29926e229010bacc5cdacca382c0b36039c
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-10 19:53:10 +0200

    test: low-items

---
+++ __tests__/low-items.spec.js
@@ -0,0 +1,39 @@
+import 'react-native';
+import React from 'react';
+import LowItems from '../components/low-items';
+import renderer from 'react-test-renderer';
+
+test('LowItems (default props) => renders correctly', () => {
+  const tree = renderer.create(
+    <LowItems />
+  ).toJSON();
+  expect(tree).toMatchSnapshot()
+})
+
+test('LowItems (with red item) => renders correctly', () => {
+  const tree = renderer.create(
+    <LowItems items={{red: true}} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot()
+})
+
+test('LowItems (with green item) => renders correctly', () => {
+  const tree = renderer.create(
+    <LowItems items={{green: true}} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot()
+})
+
+test('LowItems (with blue item) => renders correctly', () => {
+  const tree = renderer.create(
+    <LowItems items={{blue: true}} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot()
+})
+
+test('LowItems (with red, green and blue items) => renders correctly', () => {
+  const tree = renderer.create(
+    <LowItems items={{red: true, green: true, blue: true}} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot()
+})
