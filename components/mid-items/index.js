760b04531425871150d724dfee9a1765353efd25
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-10 20:15:48 +0200

    style: remove extraneous imported modules

--- components/mid-items/index.js
+++ components/mid-items/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { AppRegistry, View, Text } from 'react-native'
+import React from 'react'
+import { View } from 'react-native'
 import Item from '../item'
 import styles from './styles'
 
