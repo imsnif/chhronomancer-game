f615c1a928659119eb5d42d865000f9286012a83
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-14 19:36:39 +0200

    refactor: menu-button

--- components/menu-button/index.js
+++ components/menu-button/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { AppRegistry, View, TouchableHighlight, StyleSheet, Text } from 'react-native'
+import { View, TouchableHighlight, StyleSheet, Text } from 'react-native'
 import Button from 'react-native-button'
+import styles from './styles'
 
 export default class MenuButton extends Component {
   constructor (props) {
@@ -15,7 +16,7 @@ export default class MenuButton extends Component {
   }
   render() {
     return (
-      <View style={{width: '100%', flex: 1, margin: 7}}>
+      <View style={styles.buttonContainer}>
         <TouchableHighlight
           onPress={() => {}}
           underlayColor='#02ffff'
@@ -29,34 +30,3 @@ export default class MenuButton extends Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  text: {
-    // fontSize: 10,
-    color: '#02ffff',
-    textAlign: 'center',
-    paddingTop: 12
-  },
-  textPress: {
-    // fontSize: 10,
-    color: '#2e1a29',
-    textAlign: 'center',
-    paddingTop: 12
-  },
-  button: {
-    width: '100%',
-    flex: 1,
-    borderRadius: 2,
-    borderColor: '#02ffff',
-    backgroundColor: '#2f3241',
-    borderWidth: 2
-  },
-  buttonPress: {
-    width: '100%',
-    flex: 1,
-    borderRadius: 2,
-    backgroundColor: '#02ffff',
-    borderColor: '#02ffff',
-    borderWidth: 1
-  }
-})
---
+++ components/menu-button/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  buttonContainer: {
+    width: '100%',
+    flex: 1,
+    margin: 7
+  },
+  text: {
+    color: '#02ffff',
+    textAlign: 'center',
+    paddingTop: 12
+  },
+  textPress: {
+    color: '#2e1a29',
+    textAlign: 'center',
+    paddingTop: 12
+  },
+  button: {
+    width: '100%',
+    flex: 1,
+    borderRadius: 2,
+    borderColor: '#02ffff',
+    backgroundColor: '#2f3241',
+    borderWidth: 2
+  },
+  buttonPress: {
+    width: '100%',
+    flex: 1,
+    borderRadius: 2,
+    backgroundColor: '#02ffff',
+    borderColor: '#02ffff',
+    borderWidth: 1
+  }
+})
