80c852e395bf01b2c5a166aa76acf00dcd330810
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-14 19:00:53 +0200

    refactor: change presentational components to functions

--- components/info-box/index.js
+++ components/info-box/index.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View, Text, Image } from 'react-native'
 import styles from './styles'
 
-export default class InfoBox extends Component {
-  render () {
-    const title = this.props.title
-    const image = this.props.image
-    return (
-      <View style={styles.outerBox}>
-        <View style={styles.titleBar}>
-          <Image source={image} style={styles.imageBox} />
-          <Text style={styles.titleText}>{title}</Text>
-        </View>
-        <View style={styles.contentBox}>{this.props.children}</View>
+export default function InfoBox (props) {
+  const title = props.title
+  const image = props.image
+  return (
+    <View style={styles.outerBox}>
+      <View style={styles.titleBar}>
+        <Image source={image} style={styles.imageBox} />
+        <Text style={styles.titleText}>{title}</Text>
       </View>
-    )
-  }
+      <View style={styles.contentBox}>{props.children}</View>
+    </View>
+  )
 }
--- components/power/index.js
+++ components/power/index.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View, Text } from 'react-native'
 import ProgressBar from '../progress-bar'
 import styles from './styles'
 
-export default class Power extends Component {
-  render () {
-    return (
-      <ProgressBar progress={this.props.progress}>
-        <Text style={styles.powerName}>{this.props.name}...</Text>
-        <Text style={styles.timeLeft}>{this.props.timeLeft}</Text>
-        <View style={styles.involvedPlayers}>
-          <Text style={styles.alliedPlayers}>{this.props.alliedPlayers}</Text>
-          <Text style={styles.divider}>/</Text>
-          <Text style={styles.enemyPlayers}>{this.props.enemyPlayers}</Text>
-        </View>
-      </ProgressBar>
-    )
-  }
+export default function Power (props) {
+  return (
+    <ProgressBar progress={props.progress}>
+      <Text style={styles.powerName}>{props.name}...</Text>
+      <Text style={styles.timeLeft}>{props.timeLeft}</Text>
+      <View style={styles.involvedPlayers}>
+        <Text style={styles.alliedPlayers}>{props.alliedPlayers}</Text>
+        <Text style={styles.divider}>/</Text>
+        <Text style={styles.enemyPlayers}>{props.enemyPlayers}</Text>
+      </View>
+    </ProgressBar>
+  )
 }
