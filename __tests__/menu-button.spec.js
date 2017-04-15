e78766a1c01a2739e9e45005456f70ccca348e5c
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-15 16:48:42 +0200

    test: menubutton finger held down

--- __tests__/menu-button.spec.js
+++ __tests__/menu-button.spec.js
@@ -1,6 +1,7 @@
 import 'react-native'
 import React from 'react'
 import MenuButton from '../components/menu-button'
+import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 jest.mock('mobx-react/native', () => require('mobx-react/custom'))
@@ -17,4 +18,14 @@ test('MenuButton (with title) => renders correctly', () => {
     <MenuButton title='foo' />
   ).toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+test('MenuButton => changes color while finger is held down', () => {
+  const wrapper = shallow(
+    <MenuButton title='foo' />
+  )
+  wrapper.find('TouchableHighlight').simulate('showUnderlay')
+  expect(wrapper.find('TouchableHighlight').props().style).toMatchSnapshot()
+  wrapper.find('TouchableHighlight').simulate('hideUnderlay')
+  expect(wrapper.find('TouchableHighlight').props().style).toMatchSnapshot()
 })
