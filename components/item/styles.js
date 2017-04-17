333f8c66d2c268a10199b4334f5c91d268945e47
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-18 00:05:59 +0200

    refactor: item style performance

--- components/item/styles.js
+++ components/item/styles.js
@@ -3,52 +3,15 @@ import { StyleSheet } from 'react-native'
 import common from '../common/styles'
 
 export default StyleSheet.create({
-  boxStyle: {
-    margin: 4,
-    height: 14,
-    width: 14,
-    borderWidth: 1,
-    borderRadius: common.borderRadius,
-    backgroundColor: common.backGround,
-    alignItems: 'center'
-  },
   diamond: {
-    marginTop: 2,
-    width: 8,
-    height: 8,
+    margin: 4,
+    width: 9,
+    height: 9,
+    backgroundColor: common.backGround,
+    borderColor: common.backGround,
     borderWidth: 1,
-    borderColor: 'red',
-    backgroundColor: 'black',
     transform: [
       {rotate: '45deg'}
     ]
-  },
-  diamondShield: {
-    width: 10,
-    height: 10
-  },
-  diamondShieldTop: {
-    width: 0,
-    height: 0,
-    borderTopWidth: 5,
-    borderTopColor: 'transparent',
-    borderLeftColor: 'transparent',
-    borderLeftWidth: 5,
-    borderRightColor: 'transparent',
-    borderRightWidth: 5,
-    borderBottomColor: 'red',
-    borderBottomWidth: 2,
-  },
-  diamondShieldBottom: {
-    width: 0,
-    height: 0,
-    borderTopWidth: 7,
-    borderTopColor: 'red',
-    borderLeftColor: 'transparent',
-    borderLeftWidth: 5,
-    borderRightColor: 'transparent',
-    borderRightWidth: 5,
-    borderBottomColor: 'transparent',
-    borderBottomWidth: 5,
   }
 })
