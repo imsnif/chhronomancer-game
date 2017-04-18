9eb21b9f5cc786247aeeaca20c8d0c930f8a6209
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-18 20:40:24 +0200

    fix: change colors

--- components/common/styles.js
+++ components/common/styles.js
@@ -1,7 +1,7 @@
 export default {
   borderRadius: 0,
-  foreGround: '#00ff00',
-  backGround: 'black',
-  altText: 'black',
+  foreGround: '#329932',
+  backGround: '#141313',
+  altText: '#0e0e0e',
   font: 'telegrama_raw'
 }
