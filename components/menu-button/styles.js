1c70ac4c06b7b40f284e1e8e5818238db6243f79
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-22 16:00:14 +0200

    feat: style adjustments

--- components/menu-button/styles.js
+++ components/menu-button/styles.js
@@ -4,26 +4,41 @@ import common from '../common/styles'
 
 export default StyleSheet.create({
   buttonContainer: {
+    height: '100%',
     width: '100%',
     flex: 1,
-    margin: 7
+    marginLeft: 2,
+    marginRight: 2
   },
   text: {
     fontFamily: 'telegrama_raw',
     color: common.foreGround,
     textAlign: 'center',
-    paddingTop: 9,
+    paddingTop: 13,
     fontSize: 20
   },
   textPress: {
     fontFamily: 'telegrama_raw',
     color: common.altText,
     textAlign: 'center',
-    paddingTop: 9,
+    paddingTop: 13,
     fontSize: 20
   },
   button: {
+    margin: 2,
+    height: '100%',
     width: '100%',
+    alignSelf: 'center',
+    flex: 1,
+    borderRadius: common.borderRadius,
+    borderColor: common.foreGround,
+    backgroundColor: common.backGround,
+    borderWidth: 1
+  },
+  buttonSwitch: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
     flex: 1,
     borderRadius: common.borderRadius,
     borderColor: common.foreGround,
@@ -31,11 +46,24 @@ export default StyleSheet.create({
     borderWidth: 1
   },
   buttonPress: {
+    margin: 2,
+    height: '100%',
     width: '100%',
+    alignSelf: 'center',
     flex: 1,
     borderRadius: common.borderRadius,
-    backgroundColor: common.foreGround,
     borderColor: common.foreGround,
+    backgroundColor: common.foreGround,
+    borderWidth: 1
+  },
+  buttonPressSwitch: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    flex: 1,
+    borderRadius: common.borderRadius,
+    borderColor: common.foreGround,
+    backgroundColor: common.foreGround,
     borderWidth: 1
   }
 })
