b3374b9c19c54d886b7e5f32b12131000d8e904f
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-22 16:04:32 +0200

    feat: timelineStore sortBy

--- stores/timeline.js
+++ stores/timeline.js
@@ -1,12 +1,17 @@
 import {observable} from 'mobx'
 
 class TimelineStore {
+  @observable sortBy = 'player'
   @observable players = []
   addPlayer (id) {
     this.players.push(id)
   }
   removePlayer (id) {
     this.players = this.players.filter(p => p !== id)
+  }
+  changeSort (sort) {
+    // TODO: test this
+    this.sortBy = sort
   }
 }
 
