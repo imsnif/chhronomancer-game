81e3f06072e5b81191308e2a7b024835050711f2
Aram Drevekenin <grimsniffer@gmail.com>
2017-04-24 21:30:04 +0200

    feat: timeline-summary component

---
+++ components/timeline-summary/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { observer } from 'mobx-react/native'
+import { View, Text, Image } from 'react-native'
+import InfoBox from '../info-box'
+import styles from './styles'
+
+import steal from '../../assets/items/steal-green.png'
+import assistPrevent from '../../assets/items/assist-prevent-green.png'
+import reset from '../../assets/items/reset-green.png'
+
+@observer
+export default class TimelineSummary extends Component {
+  render() {
+    const images = {steal, assistPrevent, reset}
+    const timelineStore = this.props.timelineStore
+    const playersStore = this.props.playersStore
+    const timeline = timelineStore.getTimeline(this.props.name)
+    const imageToRender = images[timeline.type]
+    return (
+      <InfoBox title={this.props.name} image={imageToRender}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+        {
+          timeline.players.map(pId => {
+            const player = playersStore.players.find(p => p.id === pId)
+            return <Image key={player.name} style={styles.image} source={player.image} />
+          })
+        }
+        </View>
+      </InfoBox>
+    )
+  }
+}
---
+++ components/timeline-summary/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  image: {
+    margin: 4,
+    width: 30,
+    height: 30
+  }
+})
