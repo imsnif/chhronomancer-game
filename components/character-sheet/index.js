de1a864c68bf2620783e04d596e70c68640cf373
Aram Drevekenin <grimsniffer@gmail.com>
2017-06-01 15:29:08 +0300

    fix: make iterations on character sheet scrollable

--- components/character-sheet/index.js
+++ components/character-sheet/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { observer } from 'mobx-react/native'
-import { View, Text, Image, TouchableHighlight } from 'react-native'
+import { View, ScrollView, Text, Image, TouchableHighlight } from 'react-native'
 import ListBox from '../list-box'
 import Power from '../power'
 import styles from './styles'
@@ -107,37 +107,39 @@ export default class CharacterSheet extends Component {
           </View>
           <View style={{flex: 1, marginTop: 2}}>
             <ListBox title='Iterations'>
-              {
-                playerPowers.map((power, index) => {
-                  const timelineName = power.timelineName
-                  const powerName = power.name
-                  const progress = powerStore.getProgress(playerId, timelineName)
-                  const timeLeft = powerStore.getTimeLeft(playerId, timelineName)
-                  const timeline = timelineStore.getTimeline(timelineName)
-                  const type = timeline.type
-                  return(
-                    <TouchableHighlight key={index} onPress={() => this.navigate(playerId, timelineName)}>
-                      <View style={{
-                        height: 25,
-                        flexDirection: 'column'
-                      }}>
-                        <View style={{flex: 1, marginTop: 5}}>
-                          <Text style={styles.nameTextHalfCenter}>{timelineName}</Text>
+              <ScrollView>
+                {
+                  playerPowers.map((power, index) => {
+                    const timelineName = power.timelineName
+                    const powerName = power.name
+                    const progress = powerStore.getProgress(playerId, timelineName)
+                    const timeLeft = powerStore.getTimeLeft(playerId, timelineName)
+                    const timeline = timelineStore.getTimeline(timelineName)
+                    const type = timeline.type
+                    return(
+                      <TouchableHighlight key={index} onPress={() => this.navigate(playerId, timelineName)}>
+                        <View style={{
+                          height: 25,
+                          flexDirection: 'column'
+                        }}>
+                          <View style={{flex: 1, marginTop: 5}}>
+                            <Text style={styles.nameTextHalfCenter}>{timelineName}</Text>
+                          </View>
+                          <View style={{flex: 1}}>
+                            <Power
+                              name={power.name}
+                              progress={progress.get()}
+                              timeLeft={timeLeft.get()}
+                              alliedPlayers={power.alliedPlayers}
+                              enemyPlayers={power.enemyPlayers}
+                            />
+                          </View>
                         </View>
-                        <View style={{flex: 1}}>
-                          <Power
-                            name={power.name}
-                            progress={progress.get()}
-                            timeLeft={timeLeft.get()}
-                            alliedPlayers={power.alliedPlayers}
-                            enemyPlayers={power.enemyPlayers}
-                          />
-                        </View>
-                      </View>
-                    </TouchableHighlight>
-                  )
-                })
-              }
+                      </TouchableHighlight>
+                    )
+                  })
+                }
+              </ScrollView>
             </ListBox>
           </View>
         </View>
