68ac53a307db37e342ddc992a8724715543c2a2d
Aram Drevekenin <grimsniffer@gmail.com>
2017-07-03 16:29:10 +0200

    test: bidding

--- __tests__/bidding.spec.js
+++ __tests__/bidding.spec.js
@@ -6,6 +6,13 @@ import '../server-mock'
 jest.mock('mobx-react/native', () => require('mobx-react/custom'))
 jest.useFakeTimers()
 
+afterEach(() => {
+  const clockStore = require('../stores/clock').default
+  jest.useRealTimers()
+  clockStore.destroy()
+  jest.useFakeTimers()
+})
+
 test('Bidding (default props) => renders correctly', () => {
   const Bidding = require('../components/bidding').default
   const tree = renderer.create(
