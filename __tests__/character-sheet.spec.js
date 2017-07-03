4b45a8b40ce8c5cf445fefa534c913e7e1844288
Aram Drevekenin <grimsniffer@gmail.com>
2017-07-03 16:33:20 +0200

    test: character-sheet

---
+++ __tests__/character-sheet.spec.js
@@ -0,0 +1,22 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import '../server-mock'
+
+jest.mock('mobx-react/native', () => require('mobx-react/custom'))
+jest.useFakeTimers()
+
+afterEach(() => {
+  const clockStore = require('../stores/clock').default
+  jest.useRealTimers()
+  clockStore.destroy()
+  jest.useFakeTimers()
+})
+
+test('Character-Sheet (default props) => renders correctly', () => {
+  const CharacterSheet = require('../components/character-sheet').default
+  const tree = renderer.create(
+    <CharacterSheet playerId={1} />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
