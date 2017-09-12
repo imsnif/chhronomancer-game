479827a1532eae88de6842b266a2081bb17d67d1
Aram Drevekenin <grimsniffer@gmail.com>
2017-09-13 00:31:29 +0200

    test: main component

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,8 +1,15 @@
 import 'react-native'
-import '../server-mock'
 import React from 'react'
-import Chronomancer from '../components/main'
 import renderer from 'react-test-renderer'
+
+function mockFB () { // TODO: move to utils
+  const login = require('react-native-facebook-login')
+  const FBLogin = jest.fn()
+  FBLogin.mockReturnValue(null)
+  login.FBLogin = FBLogin
+  login.FBLoginManager = {LoginBehaviors: {Web: 2}}
+  return FBLogin
+}
 
 jest.mock('mobx-react/native', () => require('mobx-react/custom'))
 
@@ -18,8 +25,38 @@ afterEach(() => {
 })
 
 test('Main component (with server mock) => renders correctly', () => {
+  const Chronomancer = require('../components/main').default
+  const statsStore = require('../stores/stats').default
+  require('../server-mock') // TODO: export a function like a normal Human being
+  statsStore.changePlayerId(1)
   const tree = renderer.create(
     <Chronomancer />
   ).toJSON()
   expect(tree).toMatchSnapshot()
+})
+
+test('Main component (with server mock) => renders correctly - Waiting For Game', () => {
+  const Chronomancer = require('../components/main').default
+  const statsStore = require('../stores/stats').default
+  require('../server-mock')
+  statsStore.connect()
+  statsStore.changePlayerId('foo')
+  const tree = renderer.create(
+    <Chronomancer />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Main component (with server mock) => renders correctly - Login page', () => {
+  jest.resetModules()
+  const statsStore = require('../stores/stats').default
+  const Chronomancer = require('../components/main').default
+  const FBLogin = mockFB()
+  require('../server-mock')
+  statsStore.changePlayerId('foo')
+  statsStore.disconnect()
+  renderer.create(
+    <Chronomancer />
+  ).toJSON()
+  expect(FBLogin.mock.calls).toMatchSnapshot()
 })
