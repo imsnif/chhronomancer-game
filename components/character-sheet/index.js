221123092e22700fad03ac6a8a354186121b1a71
Aram Drevekenin <grimsniffer@gmail.com>
2017-09-15 00:28:57 +0200

    style: remove comment

--- components/character-sheet/index.js
+++ components/character-sheet/index.js
@@ -13,7 +13,7 @@ export default class CharacterSheet extends Component {
   render () {
     const playerId = statsStore.playerId
     const player = playerStore.getPlayer(playerId)
-    if (!player) return null // TODO: change route
+    if (!player) return null
     const actions = player.actions
     const items = player.items.reduce((memo, item) => {
       const existingItem = memo.find(i => i.name === item.name)
