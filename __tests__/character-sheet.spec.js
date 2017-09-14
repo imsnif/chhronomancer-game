a57bdf831557a4a880d9a695b7719ef88e0116b1
Aram Drevekenin <grimsniffer@gmail.com>
2017-09-15 00:29:32 +0200

    test: character-sheet no player

--- __tests__/character-sheet.spec.js
+++ __tests__/character-sheet.spec.js
@@ -1,7 +1,6 @@
 import 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
-import '../server-mock'
 
 jest.mock('mobx-react/native', () => require('mobx-react/custom'))
 jest.useFakeTimers()
@@ -14,7 +13,19 @@ afterEach(() => {
 })
 
 test('Character-Sheet (default props) => renders correctly', () => {
+  require('../server-mock')
   const CharacterSheet = require('../components/character-sheet').default
+  const tree = renderer.create(
+    <CharacterSheet />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Character-Sheet (no player) => renders empty', () => {
+  require('../server-mock')
+  const CharacterSheet = require('../components/character-sheet').default
+  const statsStore = require('../stores/stats').default
+  statsStore.changePlayerId('foo')
   const tree = renderer.create(
     <CharacterSheet />
   ).toJSON()
