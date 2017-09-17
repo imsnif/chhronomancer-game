9e5ad75db50c661300e791cd0cb39c70d5f4a032
Aram Drevekenin <grimsniffer@gmail.com>
2017-09-18 00:01:41 +0200

    test: player component

---
+++ __tests__/player-component.spec.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import 'react-native'
+import renderer from 'react-test-renderer'
+import { navigatorMock } from '../test-utils'
+
+jest.mock('mobx-react/native', () => require('mobx-react/custom'))
+
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
+afterEach(() => {
+  const clockStore = require('../stores/clock').default
+  jest.useRealTimers()
+  clockStore.destroy()
+  jest.useFakeTimers()
+})
+
+test('Player component => renders correctly', () => {
+  const timelineName = 'Timeline 1'
+  const playerId = 1
+  const Player = require('../components/player').default
+  require('../server-mock') // TODO: export a function like a normal Human being
+  const tree = renderer.create(
+    <Player timelineName={timelineName} id={playerId} navigator={navigatorMock()} />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
