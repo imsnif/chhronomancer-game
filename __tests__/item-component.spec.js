8b9d5efb16e3f05cb778104cd94e4be1f504472d
Aram Drevekenin <grimsniffer@gmail.com>
2017-09-28 23:53:41 +0200

    test: item onPress

--- __tests__/item-component.spec.js
+++ __tests__/item-component.spec.js
@@ -9,3 +9,11 @@ test('Item component => renders correctly', () => {
   ).toJSON()
   expect(tree).toMatchSnapshot()
 })
+
+test('Item component => renders correctly with onPress', () => {
+  const Item = require('../components/item').default
+  const tree = renderer.create(
+    <Item fill bright name='assist' onPress={() => {}} />
+  ).toJSON()
+  expect(tree).toMatchSnapshot()
+})
