a5df1cd26a4bb230a6ec59aa34aa33754b1e089a
Aram Drevekenin <grimsniffer@gmail.com>
2017-12-11 23:12:35 +0100

    fix: do not create closures in timeline

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -17,6 +17,45 @@ import { responsiveFontSize } from 'react-native-responsive-dimensions'
 
 @observer
 export default class Timeline extends Component {
+  constructor (props) {
+    super(props)
+    this._resetTimeline = this._resetTimeline.bind(this)
+    this._quest = this._quest.bind(this)
+    this._lockTimeline = this._lockTimeline.bind(this)
+    this._unlockTimeline = this._unlockTimeline.bind(this)
+    this._joinTimeline = this._joinTimeline.bind(this)
+    this._popCombineModal = this._popCombineModal.bind(this)
+    this._changeSortToPlayer = this._changeSortToPlayer.bind(this)
+    this._changeSortToType = this._changeSortToType.bind(this)
+    this._changeSortToTime = this._changeSortToTime.bind(this)
+  }
+  _resetTimeline () {
+    timelineStore.resetTimeline(this.props.name)
+  }
+  _quest () {
+    timelineStore.quest(this.props.name)
+  }
+  _lockTimeline () {
+    timelineStore.lockTimeline(this.props.name)
+  }
+  _unlockTimeline () {
+    timelineStore.unlockTimeline(this.props.name)
+  }
+  _joinTimeline () {
+    timelineStore.joinTimeline(this.props.name)
+  }
+  _popCombineModal () {
+    timelineStore.addModal(this.props.name, {modalType: 'combine'})
+  }
+  _changeSortToPlayer () {
+    timelineStore.changeTimelineSort(this.props.name, 'player')
+  }
+  _changeSortToType () {
+    timelineStore.changeTimelineSort(this.props.name, 'type')
+  }
+  _changeSortToTime () {
+    timelineStore.changeTimelineSort(this.props.name, 'time')
+  }
   render () {
     const { name } = this.props
     const timeline = timelineStore.getTimeline(name)
@@ -40,14 +79,14 @@ export default class Timeline extends Component {
                 fontSize={responsiveFontSize(3)}
                 style={styles.leftButton}
                 title='Reset'
-                onPress={() => timelineStore.resetTimeline(name)}
+                onPress={this._resetTimeline}
                 disabled={!playerInTimeline || !playerStore.hasItem(statsStore.playerId, 'reset').get()}
               />
               <MenuButton
                 fontSize={responsiveFontSize(3)}
                 style={styles.rightButton}
                 title='Quest'
-                onPress={() => timelineStore.quest(name)}
+                onPress={this._quest}
                 disabled={!playerInTimeline}
               />
             </View>
@@ -56,33 +95,33 @@ export default class Timeline extends Component {
                 fontSize={responsiveFontSize(3)}
                 style={styles.leftButton}
                 title='Lock'
-                onPress={() => timelineStore.lockTimeline(name)}
+                onPress={this._lockTimeline}
                 disabled={!playerInTimeline || !playerStore.hasItem(statsStore.playerId, 'lock').get() || timeline.isLocked}
               />
               <MenuButton
                 fontSize={responsiveFontSize(3)}
                 style={styles.rightButton}
                 title='Unlock'
-                onPress={() => timelineStore.unlockTimeline(name)}
+                onPress={this._unlockTimeline}
                 disabled={!playerInTimeline || !playerStore.hasItem(statsStore.playerId, 'unlock').get() || !timeline.isLocked}
               />
             </View>
             <View style={styles.middleButtonRow}>
-              <MenuButton fontSize={responsiveFontSize(3)} title='Combine Items' syncAction onPress={() => timelineStore.addModal(name, {modalType: 'combine'})} />
+              <MenuButton fontSize={responsiveFontSize(3)} title='Combine Items' syncAction onPress={this._popCombineModal} />
             </View>
             <View style={styles.middleButtonRow}>
               <MenuButton
                 fontSize={responsiveFontSize(3)}
                 title='Travel Here'
-                onPress={() => timelineStore.joinTimeline(name)}
+                onPress={this._joinTimeline}
                 disabled={playerInTimeline || timeline.isLocked}
               />
             </View>
           </View>
           <Switch selected={sortIndex} options={[
-            {text: 'player', action: () => timelineStore.changeTimelineSort(name, 'player')},
-            {text: 'type', action: () => timelineStore.changeTimelineSort(name, 'type')},
-            {text: 'time', action: () => timelineStore.changeTimelineSort(name, 'time')}
+            {text: 'player', action: this._changeSortToPlayer},
+            {text: 'type', action: this._changeSortToType},
+            {text: 'time', action: this._changeSortToTime}
           ]} />
         </ControlPanel>
       </View>
